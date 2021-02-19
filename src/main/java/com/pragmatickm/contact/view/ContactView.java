5e85a28fe9cc8b0de04f1a9102279760ea42ab18
AO Industries, Inc <support@aoindustries.com>
2021-02-19 09:15:28 -0600

    Renamed Html to Document

--- src/main/java/com/pragmatickm/contact/view/ContactView.java
+++ src/main/java/com/pragmatickm/contact/view/ContactView.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-contact-view - SemanticCMS view of all contacts in the current page and all children.
- * Copyright (C) 2016, 2017, 2020  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,7 +22,7 @@
  */
 package com.pragmatickm.contact.view;
 
-import com.aoindustries.html.Html;
+import com.aoindustries.html.Document;
 import com.aoindustries.servlet.http.Dispatcher;
 import com.pragmatickm.contact.model.Contact;
 import com.semanticcms.core.controller.PageUtils;
@@ -107,7 +107,7 @@ public class ContactView extends View {
 	}
 
 	@Override
-	public void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Html html, Page page) throws ServletException, IOException, SkipPageException {
+	public void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Document document, Page page) throws ServletException, IOException, SkipPageException {
 		Dispatcher.include(
 			servletContext,
 			JSPX_TARGET,
